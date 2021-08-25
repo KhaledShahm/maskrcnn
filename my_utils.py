61270bccb080d8b844b78806e62cd3bf4d5e16ad
Khaled Shahm <khaled.shahm1234@gmail.com>
2021-08-25 10:04:36 +0200

    init

--- my_utils.py
+++ my_utils.py
@@ -17,6 +17,7 @@ def get_model_instance_segmentation(num_classes):
 
     # get number of input features for the classifier
     in_features = model.roi_heads.box_predictor.cls_score.in_features
+    print(in_features)
     # replace the pre-trained head with a new one
     model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
 
