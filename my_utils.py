d03873a329926f0644dbe94e6e2b1e49da8478cf
Khaled Shahm <khaled.shahm1234@gmail.com>
2021-08-25 10:28:28 +0200

    init

--- my_utils.py
+++ my_utils.py
@@ -17,12 +17,13 @@ def get_model_instance_segmentation(num_classes):
 
     # get number of input features for the classifier
     in_features = model.roi_heads.box_predictor.cls_score.in_features
-    print(in_features)
+
     # replace the pre-trained head with a new one
     model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
 
     # now get the number of input features for the mask classifier
     in_features_mask = model.roi_heads.mask_predictor.conv5_mask.in_channels
+    print(in_features_mask)
     hidden_layer = 256
     # and replace the mask predictor with a new one
     model.roi_heads.mask_predictor = MaskRCNNPredictor(in_features_mask,
